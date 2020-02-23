73594804b748a4f019246b471e5a7c61eff4c25a
Jerome <jeertmans@icloud.com>
2020-02-24 00:26:27 +0100

    First files & decentered coef.

---
+++ homework1/homework.c
@@ -0,0 +1,3 @@
+#include <thomas.h>
+#include <homework.h>
+
---
+++ homework1/homework.h
@@ -0,0 +1,8 @@
+#ifndef homework_h
+#define homework_h
+
+#define DDCD_COEF {0.16666666666666674, -1.0, 0.5, 0.3333333333333333}
+#define DDCD_ORDER 3
+#define DDCD_DENOM 12.0
+
+#endif /* homework_h */
